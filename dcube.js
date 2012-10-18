088c1551ec15e18fb58fc59bbc361d8663fa6148
rahul <rahulsidhu@gmail.com>
2012-10-19 03:40:00 +0530

    first step to a standard data representation

--- dcube.js
+++ dcube.js
@@ -3,11 +3,11 @@
   var makeBarChart = function (data, id) {
     
     var barWidth = 40;
-    var width = (barWidth + 10) * data.length;
+    var width = (barWidth + 10) * data.array.length;
     var height = 200;
 
-    var x = d3.scale.linear().domain([0, data.length]).range([0, width]);
-    var y = d3.scale.linear().domain([0, d3.max(data, function(datum) { return datum.books; })]).
+    var x = d3.scale.linear().domain([0, data.array.length]).range([0, width]);
+    var y = d3.scale.linear().domain([0, d3.max(data.array)]).
       rangeRound([0, height]);
 
     // add the canvas to the DOM
@@ -17,12 +17,12 @@
       attr("height", height);
 
     barDemo.selectAll("rect").
-      data(data).
+      data(data.array).
       enter().
       append("svg:rect").
       attr("x", function(datum, index) { return x(index); }).
-      attr("y", function(datum) { return height - y(datum.books); }).
-      attr("height", function(datum) { return y(datum.books); }).
+      attr("y", function(datum) { return height - y(datum); }).
+      attr("height", function(datum) { return y(datum); }).
       attr("width", barWidth).
       attr("fill", "#2d578b");
 
