cd64c9d324c3c7d925187855392ba5551c76800c
rahul <rahulsidhu@gmail.com>
2012-10-26 01:28:50 +0530

    added dialog

--- dcube.js
+++ dcube.js
@@ -1,36 +1,78 @@
 (function ($, d3) {
 
-  var makeBarChart = function (data, id) {
+  var makeDcube = function (options, el) {
     
-    var barWidth = 40;
-    var width = (barWidth + 10) * data.array.length;
-    var height = 200;
+    var data = options.data,
 
-    var x = d3.scale.linear().domain([0, data.array.length]).range([0, width]);
-    var y = d3.scale.linear().domain([0, d3.max(data.array)]).
-      rangeRound([0, height]);
+	init = function () {
+	  launchWizard();
+	},
 
-    // add the canvas to the DOM
-    var barDemo = d3.select("#"+id).
-      append("svg:svg").
-      attr("width", width).
-      attr("height", height);
+	launchWizard = function () {
 
-    barDemo.selectAll("rect").
-      data(data.array).
-      enter().
-      append("svg:rect").
-      attr("x", function(datum, index) { return x(index); }).
-      attr("y", function(datum) { return height - y(datum); }).
-      attr("height", function(datum) { return y(datum); }).
-      attr("width", barWidth).
-      attr("fill", "#2d578b");
+	  var buttons = {}, $wiz;
 
+	  visualizations.forEach(function (v) {
+	    buttons[v.html] = function () {
+	      v.handler();
+	      $wiz.dialog('close');
+	    };
+	  });
+	    
+	  $wiz= $('<div/>').dialog({
+	    modal: true,
+	    buttons: buttons,
+	    title: "Visualization Type"
+	  });
+	},
+	
+	makeBarChart = function () {
+
+	  var barWidth = 40;
+	  var width = (barWidth + 10) * data.array.length;
+	  var height = 200;
+
+	  var x = d3.scale.linear().domain([0, data.array.length]).range([0, width]);
+	  var y = d3.scale.linear().domain([0, d3.max(data.array)]).
+	    rangeRound([0, height]);
+
+	  // add the canvas to the DOM
+	  var barDemo = d3.select(el).
+	    append("svg:svg").
+	    attr("width", width).
+	    attr("height", height);
+
+	  barDemo.selectAll("rect").
+	    data(data.array).
+	    enter().
+	    append("svg:rect").
+	    attr("x", function(datum, index) { return x(index); }).
+	    attr("y", function(datum) { return height - y(datum); }).
+	    attr("height", function(datum) { return y(datum); }).
+	    attr("width", barWidth).
+	    attr("fill", "#2d578b");
+
+	},
+	      
+	visualizations = [
+	  {
+	    name: 'BarChart',
+	    html: 'Barchart',
+	    handler: makeBarChart
+	  }
+	];     
+    
+    init();
+    return {
+      'init': init
     };
-
+  };
+  
   $.fn.extend({
     dCube: function (options) {
-      makeBarChart(options.data, $(this).attr('id'));
+      return this.each(function (idx, el) {
+	$.data(el, makeDcube(options, el));
+      });
     }
   });
 
